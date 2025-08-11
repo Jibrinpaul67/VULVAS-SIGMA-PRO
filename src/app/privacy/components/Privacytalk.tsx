87f12efe696e9e0e7eb12c4b062ecccf1f197d39
jibrin-admin <jibriinesther@gmail.com>
2025-08-11 11:49:06 +0100

    omo

--- src/app/privacy/components/Privacytalk.tsx
+++ src/app/privacy/components/Privacytalk.tsx
@@ -71,7 +71,7 @@ const Privacytalk = () => {
   You have the following rights with respect to information that VULAS holds about you. VULAS undertakes to provide you the same rights no matter where you choose to live.
 </p>
 <ul className="list-disc list-inside text-xl text-gray-600 space-y-2 mb-10">
-  <li><strong>Right to access:</strong> You have the right to access (and obtain a copy of, if required) the categories of personal information that we hold about you, including the information's source, purpose and period of processing, and the persons to whom the information is shared.</li>
+  <li><strong>Right to access:</strong> You have the right to access (and obtain a copy of, if required) the categories of personal information that we hold about you, including the information source, purpose and period of processing, and the persons to whom the information is shared.</li>
   <li><strong>Right to rectification:</strong> You have the right to update the information we hold about you or to rectify any inaccuracies. Based on the purpose for which we use your information, you can instruct us to add supplemental information about you in our database.</li>
   <li><strong>Right to erasure:</strong> You have the right to request that we delete your personal information in certain circumstances, such as when it is no longer necessary for the purpose for which it was originally collected.</li>
   <li><strong>Right to restriction of processing:</strong> You may also have the right to request to restrict the use of your information in certain circumstances, such as when you have objected to our use of your data but we need to verify whether we have overriding legitimate grounds to use it.</li>
@@ -83,20 +83,28 @@ const Privacytalk = () => {
 {/* Section 5 */}
 <h2 className="text-xl font-bold text-black mb-4">Business Transfers</h2>
 <p className="text-xl text-gray-600 mb-10">
-  We do not plan to sell our business, but if it ever happens through sale, acquisition, or merger, we will make sure the new owner is legally bound to honor our privacy commitments. You will be notified via email or a prominent notice on our site about any ownership changes, updates to how your personal information is used, and any choices you have regarding it.
+  We do not plan to sell our business, but if it ever happens through sale, acquisition, or merger, we will make sure the new owner is 
+  legally bound to honor our privacy commitments. You will be notified via email or a prominent notice on our site about any ownership 
+  changes, updates to how your personal information is used, and any choices you have regarding it.
 </p>
 
 {/* Section 6 */}
 <h2 className="text-xl font-bold text-black mb-4">Compliance with this Privacy Policy</h2>
 <p className="text-xl text-gray-600 mb-10">
-  We regularly review our processes to ensure your personal information is handled in line with this policy. If you have concerns about our privacy practices, email us at <a href="mailto:privacy@vulas.com" className="text-blue-600">privacy@vulas.com</a>. We will respond promptly and, if necessary, coordinate with relevant authorities to address your issue.
+  We regularly review our processes to ensure your personal information is handled in line with this policy. If you have concerns about our
+   privacy practices, email us at <a href="mailto:privacy@vulas.com" className="text-blue-600">privacy@vulas.com</a>. We will respond 
+   promptly and, if necessary, coordinate with relevant authorities to address your issue.
 </p>
 
 
 {/* Section 7 */}
 <h2 className="text-xl font-bold text-black mb-4">Notification of Changes</h2>
 <p className="text-xl text-gray-600 mb-10">
-  We may update this policy and will notify you via service announcements or email. Significant changes affecting your rights will come with at least 30 days notice to your primary email. If your email is not verified, you might miss these notices. If you disagree with changes, you can stop using our services within 30 days. Continued use after changes take effect means you accept the new policy. Minor changes would not be emailed, so check <a href="https://www.vulas.com/privacy" className="text-blue-600">www.vulas.com/privacy</a> periodically.
+  We may update this policy and will notify you via service announcements or email. Significant changes affecting your 
+  rights will come with at least 30 days notice to your primary email. If your email is not verified, you might miss these notices. 
+  If you disagree with changes, you can stop using our services within 30 days. Continued use after changes take effect means you accept 
+  the new policy. Minor changes would not be emailed, so check <a href="https://www.vulas.com/privacy" className="text-blue-600">
+  www.vulas.com/privacy</a> periodically.
 </p>
 
 
