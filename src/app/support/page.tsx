17c8173dddcac57d3b5c76409c9d538e01e52479
jibrin-admin <jibriinesther@gmail.com>
2025-08-11 11:43:12 +0100

    menu and footer to support

--- src/app/support/page.tsx
+++ src/app/support/page.tsx
@@ -1,10 +1,13 @@
 import HeroSection from './components/HeroSection';
+import Footer from '@/app/components/ui/footer';
+import Menu from '@/app/components/ui/menu';
 import SupportChannels from './components/SupportChannels';
 import ContactForm from './components/Contact';
 
 const SupportPage = () => {
   return (
     <div className="bg-white pt-[80px] sm:pt-[100px]">
+      <Menu />
       <div className="container mx-auto px-4">
         <HeroSection />
         <div className="my-12 md:my-16">
@@ -14,6 +17,7 @@ const SupportPage = () => {
           <ContactForm />
         </div>
       </div>
+        <Footer/>
     </div>
   );
 };
